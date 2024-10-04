e0835b2b2bcd904af79c7f6ad36432697fef4894
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-10-04 09:43:39 -0400

    update relay pi code

--- raspberry_pis/pi_4b_2/main.py
+++ raspberry_pis/pi_4b_2/main.py
@@ -3,6 +3,10 @@ import os
 from dotenv import load_dotenv
 import time
 import threading
+from ..common.topics import *
+from ..common.RPi4 import RPi4
+import sqlite3
+import json
 
 # Load environment variables
 load_dotenv()
@@ -21,29 +25,47 @@ MOSQUITTO_PORT = 1883
 
 # Topic mappings
 local_to_remote_topic = {
-    'home/door/light': 'discord_bot/door/light/alert',
+    HOME_DOOR_LIGHT: BOT_DOOR_LIGHT_ALERT,
 }
 remote_to_local_topic = {
-    'discord_bot/door/light/control': 'home/door/light/control',
+    BOT_DOOR_LIGHT_CONTROL: HOME_DOOR_LIGHT_CONTROL
 }
 
-# Define global clients
-local_client = None
-hivemq_client = None
+non_relay_local_topics = [HOME_LIVING_ROOM_TEMP]
 
-# Need callbacks for both Mosquitto/ local and HiveMQ
+conn = sqlite3.connect('local_database.db', check_same_thread=False)
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS temperature_readings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    temperature REAL,
+    humidity REAL,
+    source TEXT
+)''')
+conn.commit()
+
+pi = RPi4(device_id=0,name="relay_pi")
 
 # Callback on Mosquitto message received 
 def on_local_message(client, userdata, msg):
     topic = msg.topic
-    payload = msg.payload.decode()
     print(f"Local Mosquitto message received: {topic} -> {payload}")
 
     # If message needs to be forwarded to HiveMQ and bot
     if topic in local_to_remote_topic:
         remote_topic = local_to_remote_topic[topic]
+        payload = msg.payload.decode()
         hivemq_client.publish(remote_topic, payload)
         print(f"Forwarded message: {remote_topic} -> {payload}")
+    else:
+        # Save the reading form the living room temp sensor into database
+        if topic == HOME_LIVING_ROOM_TEMP:
+            data = json.loads(msg.payload.decode())
+            temperature = data['temperature']
+            humidity = data['humidity']
+            timestamp = data['timestamp']
+            cursor.execute("INSERT INTO temperature_readings (temperature, humidity, timestamp, source) VALUES (?, ?, ?, ?)", (temperature, humidity, timestamp, topic))
+
 
 # Callback for when a message is received on the HiveMQ broker
 def on_hivemq_message(client, userdata, msg):
@@ -56,6 +78,35 @@ def on_hivemq_message(client, userdata, msg):
         local_topic = remote_to_local_topic[topic]
         local_client.publish(local_topic, payload)
         print(f"Forwarded message to Mosquitto: {local_topic} -> {payload}")
+
+# Connection to local broker
+def on_local_connect(client, userdata, flags, rc, properties=None):
+    if rc == 0:
+        # Subscribe to relay topics
+        for local_topic in local_to_remote_topic.keys():
+            client.subscribe(local_topic)
+        
+        # Subscribe to non-relay topics
+        for local_topic in non_relay_local_topics:
+            client.subscribe(local_topic)
+    else:
+        print(f"Failed to connect to Mosquitto MQTT broker, return code {rc}")
+
+# Connection to HiveMQ broker
+def on_hivemq_connect(client, userdata, flags, rc, properties=None):
+    if rc == 0:
+        # Subscribe to relay topics
+        for remote_topic in remote_to_local_topic.keys():
+            hivemq_client.subscribe(remote_topic)
+            print(f"Subscribed to HiveMQ topic: {remote_topic}")
+            
+        # Subscribe to bot fan controls to store the command history in database
+        hivemq_client.subscribe(BOT_LIVING_ROOM_FAN_CONTROL)
+        # Subscribe to other Pi's temperature readings to store in database
+        hivemq_client.subscribe(HOME_LIVING_ROOM_TEMP)
+    else:
+        print(f"Failed to connect to HiveMQ, return code {rc}")
+
 
 # Setup MQTT client for local Mosquitto broker
 def setup_local_client():
@@ -75,6 +126,7 @@ def setup_local_client():
     # Start the Mosquitto client loop in a separate thread
     local_client.loop_start()
 
+
 # Setup MQTT client for HiveMQ
 def setup_hivemq_client():
     global hivemq_client
@@ -89,23 +141,28 @@ def setup_hivemq_client():
     hivemq_client.connect(HIVEMQ_BROKER, HIVEMQ_PORT)
     print("Connected to HiveMQ broker")
 
-    # Subscribe to the topics to relay from HiveMQ
+    # Subscribe to the topics to relay from HiveMQ to Picos
     for remote_topic in remote_to_local_topic.keys():
         hivemq_client.subscribe(remote_topic)
         print(f"Subscribed to HiveMQ topic: {remote_topic}")
+        
+    # Subscribe to bot fan controls to store the command history in database
+    hivemq_client.subscribe(BOT_LIVING_ROOM_FAN_CONTROL)
+    # Subscribe to other Pi's temperature readings to store in database
+    hivemq_client.subscribe(HOME_LIVING_ROOM_TEMP)
 
     # Start the HiveMQ client loop in a separate thread
     hivemq_client.loop_start()
 
-# Start both MQTT clients in parallel
-def start_clients():
-    setup_local_client()
-    setup_hivemq_client()
+pi.addClient("local_mosquitto", broker=MOSQUITTO_BROKER, port=MOSQUITTO_PORT, on_connect=on_local_connect, on_message=on_local_message, tls=False, client_id="Mosquitto_Client")
+pi.addClient("hivemq_client", broker=HIVEMQ_BROKER, port=HIVEMQ_PORT, on_connect=on_hivemq_connect, on_message=on_hivemq_message, tls=True, client_id="HiveMQ_Client", username=HIVEMQ_USERNAME, password=HIVEMQ_PASSWORD)
 
-    # Keep the main thread alive
-    while True:
+def main():
+    pi.startClients()
+    while True: # Keep the main thread alive while the clients run on separate ones
         time.sleep(1)
+
 
 # Run the script
 if __name__ == "__main__":
-    start_clients()
+    main()
