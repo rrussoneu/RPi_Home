f50e279fda7e111b6a81875c70a748b78447b12f
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-10-04 10:55:55 -0400

    update common pi4 code

--- raspberry_pis/common/RPi4.py
+++ raspberry_pis/common/RPi4.py
@@ -1,7 +1,8 @@
 
+from paho.mqtt.client import Client
 import paho.mqtt.client as paho
-from paho import Client, mqtt
 from Sensor import Sensor
+import ssl
 
 class RPi4:
     def __init__(self, device_id, name):
@@ -13,16 +14,16 @@ class RPi4:
     def addClient(self, client_name, broker, port, on_connect, on_message, tls=True, client_id="", username=None, password=None, protocol=paho.MQTTv5, subscriptions=[]):
         new_client = paho.Client(client_id=client_id, protocol=protocol)
         if tls:
-            new_client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+            new_client.tls_set(tls_version=ssl.PROTOCOL_TLS)
         if username and password:
-            self.new_client.username_pw_set(username=username, password=password)
+            new_client.username_pw_set(username=username, password=password)
         new_client.on_connect=on_connect
         new_client.on_message=on_message
         new_client.connect(broker, port)
         self.mqtt_clients[client_name] = new_client
 
     def startClients(self):
-        for client in self.mqtt_clients:
+        for client in self.mqtt_clients.values():
             client.loop_start()
 
     def getClient(self, name: str) -> Client:
