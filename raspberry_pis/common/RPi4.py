9320ce13d584d3f9c57cb0a2a5829547a3d08f54
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-10-04 10:11:50 -0400

    refacor pi class

--- raspberry_pis/common/RPi4.py
+++ raspberry_pis/common/RPi4.py
@@ -11,25 +11,22 @@ class RPi4:
         self.mqtt_clients = {} # Dictionary of MQTT clients
     
     def addClient(self, client_name, broker, port, on_connect, on_message, tls=True, client_id="", username=None, password=None, protocol=paho.MQTTv5, subscriptions=[]):
-        self.new_client = paho.Client(client_id=client_id, protocol=protocol)
+        new_client = paho.Client(client_id=client_id, protocol=protocol)
         if tls:
-            self.new_client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+            new_client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
         if username and password:
             self.new_client.username_pw_set(username=username, password=password)
-        self.new_client.on_connect=on_connect
-        self.new_client.on_message=on_message
-        self.new_client.connect(broker, port)
-        self.mqtt_clients[client_name] = self.new_client
+        new_client.on_connect=on_connect
+        new_client.on_message=on_message
+        new_client.connect(broker, port)
+        self.mqtt_clients[client_name] = new_client
 
     def startClients(self):
         for client in self.mqtt_clients:
             client.loop_start()
 
-    def addSensor(self, sensor: Sensor, name: str):
-        self.sensors[name] = sensor
-    
-    def sensorReadContinuous(self, sensor_name: str):
-        pass
+    def getClient(self, name: str) -> Client:
+        return self.mqtt_clients[name]
 
     def sensorRead(self, sensor_name: str, clients: dict[str, Client]):
         self.sensors[sensor_name].readData(clients)
