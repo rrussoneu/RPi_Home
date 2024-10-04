decb8c7e82e08013dd6cfe01703d69357c24e3f2
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-10-04 09:43:59 -0400

    make common source for topics

---
+++ raspberry_pis/common/topics.py
@@ -0,0 +1,13 @@
+#  A centralized directory of MQTT topics
+
+# HiveMQ
+BOT_DOOR_LIGHT_ALERT = 'discord_bot/door/light/alert'
+BOT_DOOR_LIGHT_CONTROL = 'discord_bot/door/light/control'
+BOT_LIVING_ROOM_FAN_CONTROL = 'discord_bot/livingroom/fan/control'
+
+
+# Mosquitto
+HOME_DOOR_LIGHT = 'home/door/light'
+HOME_DOOR_LIGHT_CONTROL = 'home/door/light/control'
+HOME_LIVING_ROOM_TEMP = 'home/livingroom/temp'
+
