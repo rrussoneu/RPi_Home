01fe99a7ad4d05a336f5f16bed94c41c187e8aba
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-10-07 21:47:42 -0400

    adjust bot topics

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -37,16 +37,22 @@ def on_connect(client, userdata, flags, rc, properties=None):
         print("Connected to MQTT Broker")
         client.subscribe(BOT_DOOR_LIGHT_ALERT)
         client.subscribe(BOT_LIVING_ROOM_TEMP_ALERT)
+        client.subscribe(BOT_GENERAL_ALERT)
         
     else:
         print(f"Failed to connect to MQTT Broker, return code {rc}")
 
 def on_message(client, userdata, msg):
+    # Maybe add some custom logic later hence the split up
     if msg.topic == BOT_DOOR_LIGHT_ALERT:
         alert_message = msg.payload.decode()
         print(f"Received alert: {alert_message}")
         asyncio.run_coroutine_threadsafe(send_alert(alert_message), bot.loop)
     elif msg.topic == BOT_LIVING_ROOM_TEMP_ALERT:
+        alert_message = msg.payload.decode()
+        print(f"Received alert: {alert_message}")
+        asyncio.run_coroutine_threadsafe(send_alert(alert_message), bot.loop)
+    else:
         alert_message = msg.payload.decode()
         print(f"Received alert: {alert_message}")
         asyncio.run_coroutine_threadsafe(send_alert(alert_message), bot.loop)
