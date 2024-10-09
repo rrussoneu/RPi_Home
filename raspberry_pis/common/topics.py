d0a8696d26fd79bb57aa622d5796b95d16d573dd
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-10-08 23:38:05 -0400

    add new topics

--- raspberry_pis/common/topics.py
+++ raspberry_pis/common/topics.py
@@ -7,8 +7,12 @@ BOT_LIVING_ROOM_FAN_CONTROL = 'discord_bot/livingroom/fan/control'
 BOT_LIVING_ROOM_TEMP_ALERT = 'discord_bot/livingroom/temp/alert'
 REAL_TIME_LIVING_ROOM_TEMP = 'visualize/livingroom/temp'
 BOT_GENERAL_ALERT = 'discord_bot/general/alert'
+BOT_LIVING_ROOM_LAMP_CONTROL = 'discord_bot/living_room/lamp/control'
+BOT_BEDROOM_LAMP_CONTROL = 'discord_bot/bedroom/lamp/control'
 
 # Mosquitto
 HOME_DOOR_LIGHT_POWER = 'cmnd/home/door/light/POWER'
 HOME_LIVING_ROOM_FAN = 'cmnd/home/living_room/fan/POWER'
 HOME_LIVING_ROOM_TEMP = 'local/living_room/temp'
+HOME_LIVING_ROOM_LAMP = 'cmnd/home/living_room/lamp/POWER'
+HOME_BEDROOM_LAMP = 'cmnd/home/bedroom/lamp/POWER'
