d05113ad9c19d455b3f20c701737ec5fd4abf3a2
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2025-01-16 22:46:23 -0500

    add some of dashboard header

--- dashboard/View/Dashboard/DashboardView.h
+++ dashboard/View/Dashboard/DashboardView.h
@@ -5,8 +5,41 @@
 #ifndef DASHBOARD_DASHBOARDVIEW_H
 #define DASHBOARD_DASHBOARDVIEW_H
 
+#pragma once
+#include "../BaseView.h"
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
 
-class DashboardView {
+class DashboardView : public BaseView {
+Q_OBJECT
+
+public:
+    explicit DashboardView(MainController* controller, QWidget* parent = nullptr)
+            : BaseView(controller, parent)
+    {
+        setupUi();
+    }
+
+
+private:
+    void setupUi()
+    {
+        auto* mainLayout = new QVBoxLayout(this);
+
+        // Header
+        auto* headerLabel = new QLabel("Dashboard", this);
+        headerLabel->setProperty("class", "view-header");
+        mainLayout->addWidget(headerLabel);
+
+        // Grid for device and sensor widgets
+        auto* grid = new QGridLayout();
+        mainLayout->addLayout(grid);
+
+        // Add some spacing and stretch
+        mainLayout->addStretch();
+    }
+
 
 };
 
