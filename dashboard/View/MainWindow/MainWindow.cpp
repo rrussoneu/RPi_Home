d465ae38498cd66e0687d1cf2c8a3a5643519189
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2025-01-20 22:12:28 -0500

    implement base main window

--- dashboard/View/MainWindow/MainWindow.cpp
+++ dashboard/View/MainWindow/MainWindow.cpp
@@ -1,19 +1,60 @@
 //
 // Created by Raphael Russo on 1/15/25.
 //
-
 #include "MainWindow.h"
-#include <QApplication>
-#include <QScreen>
 #include <QFile>
-#include <QStatusBar>
-#include <QMessageBox>
+#include <QApplication>
 
-MainWindow::MainWindow(MainController* controller, QWidget* parent)
-        : QMainWindow(parent)
-        , m_controller(controller)
-{
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+    setupStyle();
     setupUi();
-    setupTheme();
+}
 
+void MainWindow::setupUi() {
+    // Set window properties
+    setWindowTitle("Smart Home");
+    resize(1280, 800);
+
+    // Create navigation dock
+    m_navigationDock = new QDockWidget(this);
+    m_navigationDock->setFeatures(QDockWidget::NoDockWidgetFeatures);
+    m_navigationDock->setAllowedAreas(Qt::LeftDockWidgetArea);
+    m_navigationDock->setTitleBarWidget(new QWidget());
+    auto* navigationBar = new NavigationBar(this);
+    navigationBar->setProperty("class", "navigation-bar");
+    m_navigationDock->setWidget(navigationBar);
+    addDockWidget(Qt::LeftDockWidgetArea, m_navigationDock);
+
+    // Create central stacked widget
+    m_centralStack = new QStackedWidget(this);
+    setCentralWidget(m_centralStack);
+
+    // Create views
+    m_dashboardView = std::make_unique<DashboardView>(this);
+
+    // Add views to stack
+    m_centralStack->addWidget(m_dashboardView.get());
+
+
+    // Connect navigation signals
+    connect(navigationBar, &NavigationBar::navigationRequested,
+            m_centralStack, &QStackedWidget::setCurrentIndex);
+}
+
+void MainWindow::setupStyle() {
+    QFile styleFile(":/res/styles/default.qss");
+    if (!styleFile.open(QFile::ReadOnly | QFile::Text)) {
+        qDebug() << "Failed to open stylesheet:" << styleFile.errorString();
+        return;
+    }
+    QString style = styleFile.readAll();
+    styleFile.close();
+
+    // Apply the stylesheet
+    qApp->setStyleSheet(style);
+
+    // To debug any parsing issues
+    if (style.isEmpty()) {
+        qDebug() << "Stylesheet is empty";
+    }
 }
--- dashboard/View/MainWindow/MainWindow.h
+++ dashboard/View/MainWindow/MainWindow.h
@@ -4,48 +4,72 @@
 
 #ifndef DASHBOARD_MAINWINDOW_H
 #define DASHBOARD_MAINWINDOW_H
-
-
+#pragma once
 #include <QMainWindow>
+#include <QDockWidget>
 #include <QStackedWidget>
-#include "../../Controller/MainController.h"
+#include <QToolButton>
+#include <QVBoxLayout>
+#include <memory>
 #include "../Dashboard/DashboardView.h"
 
-class MainWindow : public QMainWindow {
 
+class NavigationBar : public QWidget {
+Q_OBJECT
 public:
-    explicit MainWindow(MainController* controller, QWidget* parent = nullptr);
-    ~MainWindow() override = default;
+    explicit NavigationBar(QWidget* parent = nullptr) : QWidget(parent) {
+        auto* layout = new QVBoxLayout(this);
+        layout->setSpacing(8);
+        layout->setContentsMargins(4, 8, 4, 8);
 
+        createNavButton(":/res/icons/dashboard.svg", "Dashboard", 0);
+        createNavButton(":/res/icons/devices.svg", "Devices", 1);
+        createNavButton(":/res/icons/garden.svg", "Garden", 2);
+        createNavButton(":/res/icons/camera.svg", "Camera", 3);
+        createNavButton(":/res/icons/settings.svg", "Settings", 4);
 
-private slots:
-    void navigateToView(int viewIndex);
-    void handleDeviceStateChanged(const QString& deviceId);
-    void handleConnectionStatus(bool connected);
-    void showSettings();
+        layout->addStretch();
+        setFixedWidth(60);
+        setProperty("class", "navigation-bar");
+    }
+
+signals:
+    void navigationRequested(int index);
+
+private:
+    void createNavButton(const QString& iconPath, const QString& tooltip, int index) {
+        auto* button = new QToolButton(this);
+        button->setIcon(QIcon(iconPath));
+        button->setIconSize(QSize(28, 28));
+        button->setToolTip(tooltip);
+        button->setCheckable(true);
+        button->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+        button->setProperty("class", "navigation-button");
+        button->setAutoExclusive(true);
+        button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        button->setFixedHeight(60);
+        if (index == 0) button->setChecked(true);
+
+        connect(button, &QToolButton::clicked, this, [this, index]() {
+            emit navigationRequested(index);
+        });
+
+        layout()->addWidget(button);
+    }
+};
+
+class MainWindow : public QMainWindow {
+Q_OBJECT
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
 
 private:
     void setupUi();
-    void createActions();
-    void createMenus();
-    void createToolBar();
-    void createStatusBar();
-    void createDockWidgets();
-    void setupTheme();
+    void setupStyle();
 
-    // Core components
-    MainController* m_controller;
+    QDockWidget* m_navigationDock;
     QStackedWidget* m_centralStack;
-    QToolBar* m_mainToolbar;
-
-    // Views
     std::unique_ptr<DashboardView> m_dashboardView;
-
-    // Actions
-    QAction* m_dashboardAction;
-    QAction* m_devicesAction;
-    QAction* m_gardenAction;
 };
-
 
 #endif //DASHBOARD_MAINWINDOW_H
